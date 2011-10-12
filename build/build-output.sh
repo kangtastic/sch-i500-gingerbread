d24bb2e891e3da2227a8009bf502c440cd7452db
kangtastic <kangscinate@gmail.com>
2011-10-11 23:45:25 -0700

    Modify buildscripts to facilitate kernel releases

--- build/build-output.sh
+++ build/build-output.sh
@@ -13,14 +13,17 @@ if [ ! -d "$OUTDIR" ]; then
 	mkdir $OUTDIR
 fi
 
-#TODO: clean up and move work into out or bin; don't work in REF
-
 # Edify Update ZIP
-cp $KERNELBINDIR/zImage $REFDIR/update-zip/kernel_update/
-pushd $REFDIR/update-zip > /dev/null
+cp -r $REFDIR/update-zip $OUTDIR/update-zip
+cp $KERNELBINDIR/zImage $OUTDIR/update-zip/kernel_update/
+# Put $DATE value into updater-script
+pushd $OUTDIR/update-zip/META-INF/com/google/android > /dev/null
+sed _\$DATE_$DATE <updater-script >updater-script.tmp
+mv -f updater-script.tmp updater-script
+popd > /dev/null
+pushd $OUTDIR/update-zip
 zip -r -q TKSGB-EH09-"$DATE.$TIME".zip .
 mv TKSGB-EH09-"$DATE.$TIME".zip $OUTDIR/
-rm -f ./kernel_update/zImage
 popd > /dev/null
 
 # ODIN tarball
@@ -30,6 +33,10 @@ pushd $OUTDIR > /dev/null
 tar --format=ustar -cf TKSGB-EH09-"$DATE.$TIME".tar zImage
 md5sum -t TKSGB-EH09-"$DATE.$TIME".tar >> TKSGB-EH09-"$DATE.$TIME".tar
 mv TKSGB-EH09-"$DATE.$TIME".tar TKSGB-EH09-"$DATE.$TIME".tar.md5
+# Another with no $TIME in filename for releases
+tar --format=ustar -cf TKSGB-EH09-"$DATE".tar zImage
+md5sum -t TKSGB-EH09-"$DATE".tar >> TKSGB-EH09-"$DATE".tar
+mv TKSGB-EH09-"$DATE".tar TKSGB-EH09-"$DATE".tar.md5
 
 # leave the output files in place for heimdall for now
 #rm -f recovery.bin
