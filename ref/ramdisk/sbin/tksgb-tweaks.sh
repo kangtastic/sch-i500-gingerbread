59817f8ea11035e1147527627127326bdb6f65c0
kangtastic <kangscinate@gmail.com>
2011-10-15 18:54:46 -0700

    zram (imoseyon) and ramdisk changes to accommodate

--- ref/ramdisk/sbin/tksgb-tweaks.sh
+++ ref/ramdisk/sbin/tksgb-tweaks.sh
@@ -1,8 +1,27 @@
 #!/system/bin/sh
 
 # Remount /system rw
-busybox mount -o remount,rw /
+busybox mount -t rootfs -o remount,rw rootfs
 busybox mount -o remount,rw /system
+
+# Install liblights
+MD5SUM1=`busybox md5sum /system/lib/hw/lights.s5pc110.so | awk '{ print \$1 }'`
+MD5SUM2=`busybox md5sum /res/lib/lights.s5pc110.so | awk '{ print \$1 }'`
+if [ ! "$MD5SUM1" = "$MD5SUM2" ]; then
+	busybox cp /system/lib/hw/lights.s5pc110.so /system/lib/hw/lights.s5pc110.so.old
+	busybox cp -f /res/lib/lights.s5pc110.so /system/lib/hw/lights.s5pc110.so
+	busybox chown root.root /system/lib/hw/lights.s5pc110.so
+	busybox chmod 0644 /system/lib/hw/lights.s5pc110.so
+fi
+
+# Install bootanimation binary
+MD5SUM1=`busybox md5sum /system/bin/bootanimation | awk '{ print \$1 }'`
+MD5SUM2=`busybox md5sum /res/lib/bootanimation | awk '{ print \$1 }'`
+if [ ! "$MD5SUM1" = "$MD5SUM2" ]; then
+	busybox cp -f /res/lib/bootanimation /system/bin/bootanimation
+	busybox chown root.shell /system/bin/bootanimation
+	busybox chmod 0755 /system/bin/bootanimation
+fi
 
 # Fix up resolv.conf with multicasted Verizon and Google DNS
 if [ ! -f "/system/etc/resolv.conf" ]; then
@@ -16,16 +35,6 @@ if [ -e /sys/devices/virtual/bdi/179:0/read_ahead_kb ]; then
 	echo "1024" > /sys/devices/virtual/bdi/179:0/read_ahead_kb
 fi
 
-# Install liblights
-busybox cp -f /res/liblights/lights.s5pc110.so /system/lib/hw/lights.s5pc110.so
-busybox chown root.root /system/lib/hw/lights.s5pc110.so
-busybox chmod 0644 /system/lib/hw/lights.s5pc110.so
-
-# Install bootanimation binary
-busybox cp -f /res/bootanimation /system/bin/bootanimation
-busybox chown root.shell /system/bin/bootanimation
-busybox chmod 0755 /system/bin/bootanimation
-
 # Bootanimation hack
 while [ 1 ]; do
 	sleep 1;
@@ -36,3 +45,5 @@ while [ 1 ]; do
 		exit;
 	fi
 done
+
+busybox mount -o remount,ro /system
