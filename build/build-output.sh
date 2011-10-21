7e42eebc2d7b6fe9b1620453626f8732a5d93905
kangtastic <kangscinate@gmail.com>
2011-10-20 23:08:44 -0700

    More of the same; change the output filename to TKSGB-I500-whatever. After all, it isn't just for EH09, it works for EI20 too.

--- build/build-output.sh
+++ build/build-output.sh
@@ -18,25 +18,26 @@ cp -r $REFDIR/update-zip $OUTDIR/update-zip
 cp $KERNELBINDIR/zImage $OUTDIR/update-zip/kernel_update/
 # Put $DATE value into updater-script
 pushd $OUTDIR/update-zip/META-INF/com/google/android > /dev/null
-sed _\$DATE_$DATE <updater-script >updater-script.tmp
+sed s_"\$DATE"_"$DATE"_ <updater-script >updater-script.tmp
 mv -f updater-script.tmp updater-script
 popd > /dev/null
 pushd $OUTDIR/update-zip
-zip -r -q TKSGB-EH09-"$DATE.$TIME".zip .
-mv TKSGB-EH09-"$DATE.$TIME".zip $OUTDIR/
+zip -r -q TKSGB-I500-"$DATE.$TIME".zip .
+mv TKSGB-I500-"$DATE.$TIME".zip $OUTDIR/
 popd > /dev/null
 
 # ODIN tarball
 cp $KERNELBINDIR/zImage $OUTDIR/
 cp $OUTDIR/zImage $OUTDIR/recovery.bin
 pushd $OUTDIR > /dev/null
-tar --format=ustar -cf TKSGB-EH09-"$DATE.$TIME".tar zImage
-md5sum -t TKSGB-EH09-"$DATE.$TIME".tar >> TKSGB-EH09-"$DATE.$TIME".tar
-mv TKSGB-EH09-"$DATE.$TIME".tar TKSGB-EH09-"$DATE.$TIME".tar.md5
-# Another with no $TIME in filename for releases
-tar --format=ustar -cf TKSGB-EH09-"$DATE".tar zImage
-md5sum -t TKSGB-EH09-"$DATE".tar >> TKSGB-EH09-"$DATE".tar
-mv TKSGB-EH09-"$DATE".tar TKSGB-EH09-"$DATE".tar.md5
+tar --format=ustar -cf TKSGB-I500-"$DATE.$TIME".tar zImage
+md5sum -t TKSGB-I500-"$DATE.$TIME".tar >> TKSGB-I500-"$DATE.$TIME".tar
+mv TKSGB-I500-"$DATE.$TIME".tar TKSGB-I500-"$DATE.$TIME".tar.md5
+# Another with no $TIME in filename for "official" releases,
+# because renaming the .tar.md5 breaks the checksum and I'm lazy
+tar --format=ustar -cf TKSGB-I500-"$DATE".tar zImage
+md5sum -t TKSGB-I500-"$DATE".tar >> TKSGB-I500-"$DATE".tar
+mv TKSGB-I500-"$DATE".tar TKSGB-I500-"$DATE".tar.md5
 
 # leave the output files in place for heimdall for now
 #rm -f recovery.bin
--- build_log.sh
+++ build_log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE=$(date +%m%d)
 TIME=$(date +%H%M)
-bash build.sh clean 2>&1 | tee TKSGB-EH09-"$DATE.$TIME".buildlog.txt
+bash build.sh clean 2>&1 | tee TKSGB-I500-"$DATE.$TIME".buildlog.txt
